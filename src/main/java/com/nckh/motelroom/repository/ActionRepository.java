f347163b94178800620757eb4c46656ae1106b09
DoPhucLam <671598@sv.vnua.edu.vn>
2024-12-11 22:44:16 +0700

    Thêm annotation bị thiếu gây ra lỗi

--- src/main/java/com/nckh/motelroom/repository/ActionRepository.java
+++ src/main/java/com/nckh/motelroom/repository/ActionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface ActionRepository extends JpaRepository<Action, Long> {
     Page<Action> findAllByUser_Id(Long id, Pageable pageable);
 }
