a81a5b0f2c672c75917f2b5567bcfe2f14245bcf
DoPhucLam <671598@sv.vnua.edu.vn>
2024-12-11 22:44:29 +0700

    .

--- src/main/java/com/nckh/motelroom/repository/ActionRepository.java
+++ src/main/java/com/nckh/motelroom/repository/ActionRepository.java
@@ -4,6 +4,7 @@ import com.nckh.motelroom.model.Action;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ActionRepository extends JpaRepository<Action, Long> {
