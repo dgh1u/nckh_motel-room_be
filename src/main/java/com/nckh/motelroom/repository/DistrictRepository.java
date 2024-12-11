397bd60cf536f08964ec0b43bc97b6cb8247b97b
DoPhucLam <671598@sv.vnua.edu.vn>
2024-12-11 22:44:37 +0700

    Thêm annotation bị thiếu gây ra lỗi

--- src/main/java/com/nckh/motelroom/repository/DistrictRepository.java
+++ src/main/java/com/nckh/motelroom/repository/DistrictRepository.java
@@ -2,6 +2,7 @@ package com.nckh.motelroom.repository;
 
 import com.nckh.motelroom.model.District;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
