ed47bb33426261343f19586f1e455011241377e0
DoPhucLam <671598@sv.vnua.edu.vn>
2024-12-11 22:44:34 +0700

    Thêm annotation bị thiếu gây ra lỗi

--- src/main/java/com/nckh/motelroom/repository/DistrictRepository.java
+++ src/main/java/com/nckh/motelroom/repository/DistrictRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@Repository
 public interface DistrictRepository extends JpaRepository<District, Long> {
     Optional<District> findDistrictById(Long id);
 }
