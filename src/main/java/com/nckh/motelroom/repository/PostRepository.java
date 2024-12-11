a14dc56b9656dab615fdd5bb7e2b87dd3b03a722
DoPhucLam <671598@sv.vnua.edu.vn>
2024-12-11 22:44:42 +0700

    Thêm annotation bị thiếu gây ra lỗi

--- src/main/java/com/nckh/motelroom/repository/PostRepository.java
+++ src/main/java/com/nckh/motelroom/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByTitle(String title);
 }
