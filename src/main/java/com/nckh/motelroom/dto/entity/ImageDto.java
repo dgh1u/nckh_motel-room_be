3c28f66a5d4675e5c49e5adc6afa857a5d93264e
DoPhucLam <671598@sv.vnua.edu.vn>
2024-12-14 11:56:03 +0700

    SỬa lại bean

--- src/main/java/com/nckh/motelroom/dto/entity/ImageDto.java
+++ src/main/java/com/nckh/motelroom/dto/entity/ImageDto.java
@@ -1,8 +1,10 @@
 package com.nckh.motelroom.dto.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ImageDto {
     private String id;
 
--- src/main/java/com/nckh/motelroom/model/Image.java
+++ src/main/java/com/nckh/motelroom/model/Image.java
@@ -3,6 +3,7 @@ package com.nckh.motelroom.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -12,11 +13,9 @@ import org.hibernate.annotations.OnDeleteAction;
 @Table(name = "image")
 public class Image {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false)
     private String id;
-
-    @Column(name = "data")
-    private byte[] data;
 
     @Column(name = "file_name")
     private String fileName;
@@ -24,9 +23,18 @@ public class Image {
     @Column(name = "file_type", length = 50)
     private String fileType;
 
+    @Column(name = "data")
+    private byte[] data;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "post_id")
     private Post post;
 
+    public Image(String fileName, String fileType, byte[] data, Post post) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.data = data;
+        this.post = post;
+    }
 }
