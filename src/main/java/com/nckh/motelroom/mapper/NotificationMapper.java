4d8366b98a55c8f37bb57a427f07a4c4a62c220c
DoPhucLam <671598@sv.vnua.edu.vn>
2024-12-21 08:57:29 +0700

    Them moi mot so class

---
+++ src/main/java/com/nckh/motelroom/mapper/NotificationMapper.java
@@ -0,0 +1,13 @@
+package com.nckh.motelroom.mapper;
+
+import com.nckh.motelroom.dto.entity.NotificationDto;
+import com.nckh.motelroom.model.Notification;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface NotificationMapper {
+    NotificationDto toNotificationDto(Notification notification);
+    Notification toNotification(NotificationDto notificationDto);
+}
