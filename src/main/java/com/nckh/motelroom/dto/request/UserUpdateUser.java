8ad937634cbe176ab806890df1db6bb2333ac4f8
khongten315315 <khongten315315@gmail.com>
2025-02-15 17:00:16 +0700

    upload avatar

--- src/main/java/com/nckh/motelroom/dto/request/UserUpdateUser.java
+++ src/main/java/com/nckh/motelroom/dto/request/UserUpdateUser.java
@@ -2,6 +2,7 @@ package com.nckh.motelroom.dto.request;
 
 import com.nckh.motelroom.constant.Constant;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,11 +10,11 @@ import lombok.Data;
 public class UserUpdateUser {
     @NotNull
     private Long id;
-
+    @NotBlank(message = "Họ tên không được để trống")
     private String fullName;
-
+    @NotBlank(message = "Địa chỉ không được để trống")
     private String address;
-
+    @NotBlank(message = "Số điện thoại không được để trống")
     private String phone;
 
     private boolean block;
--- src/main/java/com/nckh/motelroom/service/impl/UserServiceImp.java
+++ src/main/java/com/nckh/motelroom/service/impl/UserServiceImp.java
@@ -1,23 +1,62 @@
 package com.nckh.motelroom.service.impl;
 
+import com.nckh.motelroom.dto.entity.UserDto;
+import com.nckh.motelroom.exception.DataExistException;
+import com.nckh.motelroom.mapper.UserMapper;
 import com.nckh.motelroom.model.User;
 import com.nckh.motelroom.repository.UserRepository;
 import com.nckh.motelroom.repository.custom.CustomUserQuery;
 import com.nckh.motelroom.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.Base64;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImp implements UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Override
     public Page<User> getAllUser(CustomUserQuery.UserFilterParam param, PageRequest pageRequest) {
         Specification<User> specification = CustomUserQuery.getFilterUser(param);
         return userRepository.findAll(specification, pageRequest);
+    }
+
+    @Override
+    public UserDto selectUserByEmail(String email) {
+        Optional<User> userOptional=userRepository.findByEmail(email);
+        if(!userOptional.isPresent()){
+            throw new DataExistException("Email không tồn tại");
+        }
+        User user=userOptional.get();
+        return userMapper.toUserDto(user);
+    }
+
+    @Override
+    public UserDto selectUserById(Long id) {
+        Optional<User> userOptional=userRepository.findById(id);
+        if(!userOptional.isPresent()){
+            throw new DataExistException("Người dùng không tồn tại");
+        }
+        User user=userOptional.get();
+        return userMapper.toUserDto(user);
+    }
+
+    @Override
+    public void changeAvatar(String email, byte[] fileBytes) {
+        Optional<User> userOptional=userRepository.findByEmail(email);
+        if(!userOptional.isPresent()){
+            throw new DataExistException("Email không tồn tại");
+        }
+        User user=userOptional.get();
+        user.setB64(Base64.getEncoder().encodeToString(fileBytes));
+        userRepository.saveAndFlush(user);
     }
 }
